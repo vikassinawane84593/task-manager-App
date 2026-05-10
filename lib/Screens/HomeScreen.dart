4de6c69715506f0cab96ddb8027b11b411623158
Vikas Sonawane <vikassonawane@gmail.com>
2026-05-10 22:17:52 +0530

    updated

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -17,12 +17,16 @@ class HomeScreen extends StatelessWidget {
     final TaskService taskService = TaskService();
 
     return Scaffold(
+
       backgroundColor: const Color(0xfff5f5f5),
 
       appBar: AppBar(
+
         backgroundColor: Colors.red,
+
         title: const Text(
           "Task Manager",
+
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
@@ -30,12 +34,19 @@ class HomeScreen extends StatelessWidget {
         ),
 
         centerTitle: true,
+
         actions: [
+
           IconButton(
+
             onPressed: () async {
+
               await authService.logout();
+
               Navigator.pushReplacement(
+
                 context,
+
                 MaterialPageRoute(
                   builder: (context) =>
                   const LoginScreen(),
@@ -52,24 +63,37 @@ class HomeScreen extends StatelessWidget {
       ),
 
       body: Padding(
+
         padding: const EdgeInsets.all(20),
+
         child: Column(
+
           crossAxisAlignment:
           CrossAxisAlignment.start,
+
           children: [
+
             Container(
+
               width: double.infinity,
+
               padding: const EdgeInsets.all(20),
+
               decoration: BoxDecoration(
+
                 color: Colors.red.shade700,
+
                 borderRadius:
                 BorderRadius.circular(20),
               ),
 
               child: const Column(
+
                 crossAxisAlignment:
                 CrossAxisAlignment.start,
+
                 children: [
+
                   Text(
                     "Welcome Back 👋",
 
@@ -84,6 +108,7 @@ class HomeScreen extends StatelessWidget {
 
                   Text(
                     "Manage your daily tasks easily",
+
                     style: TextStyle(
                       color: Colors.white70,
                       fontSize: 16,
@@ -96,7 +121,9 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(height: 30),
 
             const Text(
+
               "Your Tasks",
+
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
@@ -106,12 +133,16 @@ class HomeScreen extends StatelessWidget {
             const SizedBox(height: 20),
 
             Expanded(
+
               child: StreamBuilder<QuerySnapshot>(
+
                 stream: taskService.getTasks(),
+
                 builder: (context, snapshot) {
 
                   if (snapshot.connectionState ==
                       ConnectionState.waiting) {
+
                     return const Center(
                       child: CircularProgressIndicator(),
                     );
@@ -121,6 +152,7 @@ class HomeScreen extends StatelessWidget {
                       snapshot.data!.docs.isEmpty) {
 
                     return Center(
+
                       child: Column(
 
                         mainAxisAlignment:
@@ -137,7 +169,9 @@ class HomeScreen extends StatelessWidget {
                           const SizedBox(height: 15),
 
                           Text(
+
                             "No Tasks Yet",
+
                             style: TextStyle(
                               fontSize: 20,
                               color: Colors.grey.shade600,
@@ -148,7 +182,9 @@ class HomeScreen extends StatelessWidget {
                           const SizedBox(height: 8),
 
                           Text(
+
                             "Tap + button to add tasks",
+
                             style: TextStyle(
                               fontSize: 15,
                               color: Colors.grey.shade500,
@@ -160,12 +196,20 @@ class HomeScreen extends StatelessWidget {
                   }
 
                   final tasks = snapshot.data!.docs;
+
                   return ListView.builder(
+
                     itemCount: tasks.length,
+
                     itemBuilder: (context, index) {
+
                       final task = tasks[index];
 
                       return Card(
+
+                        margin: const EdgeInsets.only(
+                          bottom: 15,
+                        ),
 
                         shape: RoundedRectangleBorder(
                           borderRadius:
@@ -173,17 +217,23 @@ class HomeScreen extends StatelessWidget {
                         ),
 
                         child: ListTile(
+
                           isThreeLine: true,
+
                           contentPadding:
                           const EdgeInsets.all(15),
+
                           title: Text(
+
                             task["title"],
+
                             style: const TextStyle(
                               fontWeight: FontWeight.bold,
                             ),
                           ),
 
                           subtitle: Padding(
+
                             padding:
                             const EdgeInsets.only(
                               top: 8,
@@ -195,46 +245,57 @@ class HomeScreen extends StatelessWidget {
                           ),
 
                           trailing: SingleChildScrollView(
+
                             child: Column(
+
                               mainAxisAlignment:
                               MainAxisAlignment.start,
+
                               children: [
+
                                 Text(
+
                                   task["date"],
+
                                   style: TextStyle(
                                     color: Colors.red.shade700,
                                     fontWeight:
                                     FontWeight.bold,
                                   ),
                                 ),
-                            
+
                                 Row(
-                            
+
                                   mainAxisSize:
                                   MainAxisSize.min,
+
                                   children: [
+
                                     IconButton(
-                            
+
                                       onPressed: () {
-                            
+
                                         TaskModel taskModel =
                                         TaskModel(
-                            
+
                                           id: task.id,
+
                                           title:
                                           task["title"],
+
                                           description:
                                           task["description"],
+
                                           date:
                                           task["date"],
                                         );
-                            
+
                                         Navigator.push(
-                            
+
                                           context,
-                            
+
                                           MaterialPageRoute(
-                            
+
                                             builder: (context) =>
                                                 EditTaskScreen(
                                                   task: taskModel,
@@ -242,22 +303,82 @@ class HomeScreen extends StatelessWidget {
                                           ),
                                         );
                                       },
-                            
+
                                       icon: const Icon(
                                         Icons.edit,
                                         color: Colors.blue,
                                       ),
                                     ),
-                            
+
                                     IconButton(
-                            
+
                                       onPressed: () async {
-                            
-                                        await taskService
-                                            .deleteTask(
-                                            task.id);
+
+                                        showDialog(
+
+                                          context: context,
+
+                                          builder: (context) {
+
+                                            return AlertDialog(
+
+                                              title: const Text(
+                                                "Delete Task",
+                                              ),
+
+                                              content: const Text(
+                                                "Are you sure you want to delete this task?",
+                                              ),
+
+                                              actions: [
+
+                                                TextButton(
+
+                                                  onPressed: () {
+
+                                                    Navigator.pop(context);
+                                                  },
+
+                                                  child: const Text(
+                                                    "Cancel",
+                                                  ),
+                                                ),
+
+                                                TextButton(
+
+                                                  onPressed: () async {
+
+                                                    await taskService
+                                                        .deleteTask(task.id);
+
+                                                    Navigator.pop(context);
+
+                                                    ScaffoldMessenger.of(context)
+                                                        .showSnackBar(
+
+                                                      const SnackBar(
+                                                        content: Text(
+                                                          "Task Deleted",
+                                                        ),
+                                                      ),
+                                                    );
+                                                  },
+
+                                                  child: const Text(
+
+                                                    "Delete",
+
+                                                    style: TextStyle(
+                                                      color: Colors.red,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
                                       },
-                            
+
                                       icon: const Icon(
                                         Icons.delete,
                                         color: Colors.red,
@@ -281,9 +402,13 @@ class HomeScreen extends StatelessWidget {
 
       floatingActionButton:
       FloatingActionButton(
+
         backgroundColor: Colors.red.shade700,
+
         onPressed: () {
+
           Navigator.push(
+
             context,
 
             MaterialPageRoute(
